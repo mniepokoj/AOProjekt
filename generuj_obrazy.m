177710cd5477d4124c3c06cdfb9ff22964ce8871
Luke Waj <83553257+mkdirer@users.noreply.github.com>
2022-01-15 20:01:45 +0100

    some change

--- generuj_obrazy.m
+++ generuj_obrazy.m
@@ -1,26 +1,38 @@
 pos = [0 0];
-chars1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-chars2 = ["+", "/", "*", "-"];
-folders = ["oper_addition", "oper_division", "oper_multiplication", "oper_subtraction"];
+%chars1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+%chars2 = ["+", "/", "*", "-"];
+%folders = ["oper_addition", "oper_division", "oper_multiplication", "oper_subtraction"];
 
-list_of_fonts = d(1: 45);
+chars3 = ["(", ")", "ln", "^"];
+folders2 = ["oper_left_arrow", "oper_right_arrow", "oper_ln", "oper_exponentiation"];
+
+list_of_fonts = (1: 45);
 fonts = {'Arial', 'Amiri', 'Caladea', 'Cambria', 'Candara', 'Carlito', 'Courier', 'Ebrima', 'Gadugi', 'Impact', 'Times', 'Sylfaen', 'Tahoma', 'Times New Roman', 'Verdana', 'Microsoft Sans Serif', 'Rubik-Regular', 'Gentium Basic', 'Alef Regular', 'Bahnschrift'};
 
 for j = 1: length(fonts)
-    num1 = 0;
-    for i = 1:  length(chars1)
-        im = imread('baza.png');
-        im = insertText(im, pos, chars1{i}, 'FontSize', 110, 'BoxColor', 'white', 'Font', fonts{j});
-        str = append( "number/num_", num2str(num1), "/", fonts{j}, "_", num2str(num1), ".png" );
-        imwrite(im,  str);
-        num1 = num1 + 1;
-    end
+%     num1 = 0;
+%     for i = 1:  length(chars1)
+%         im = imread('baza.png');
+%         im = insertText(im, pos, chars1{i}, 'FontSize', 110, 'BoxColor', 'white', 'Font', fonts{j});
+%         str = append( "number/num_", num2str(num1), "/", fonts{j}, "_", num2str(num1), ".png" );
+%         imwrite(im,  str);
+%         num1 = num1 + 1;
+%     end
+
+% num2 = 0;
+%     for k = 1: length(chars2)
+%         im = imread('baza.png');
+%         im = insertText(im, pos, chars2{k}, 'FontSize', 110, 'BoxColor', 'white', 'Font', fonts{j});
+%         str = append( "operations/", folders{k}, "/", fonts{j}, "_", num2str(num2), ".png" );
+%         imwrite(im,  str);
+%         num2 = num2 + 1;
+%     end
 
     num2 = 0;
-    for k = 1: length(chars2)
+    for k = 1: length(chars3)
         im = imread('baza.png');
-        im = insertText(im, pos, chars2{k}, 'FontSize', 110, 'BoxColor', 'white', 'Font', fonts{j});
-        str = append( "operations/", folders{k}, "/", fonts{j}, "_", num2str(num2), ".png" );
+        im = insertText(im, pos, chars3{k}, 'FontSize', 110, 'BoxColor', 'white', 'Font', fonts{j});
+        str = append( "operations/", folders2{k}, "/", fonts{j}, "_", num2str(num2), ".png" );
         imwrite(im,  str);
         num2 = num2 + 1;
     end
