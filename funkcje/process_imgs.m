7940632aacd5366c55d828fc69d89febd4dd3c93
kogelbezmogel <85304480+kogelbezmogel@users.noreply.github.com>
2022-01-23 11:40:10 +0100

    Zmieniono kilka rzeczy

--- funkcje/process_imgs.m
+++ funkcje/process_imgs.m
@@ -1,9 +1,9 @@
-function w = wczytaj(ids)
-    len = get_length(ids);
+function res = process_imgs(imgs)
+    len = get_length(imgs);
     fun = {@AO5RBlairBliss, @AO5RCircularityL, @AO5RCircularityS, @AO5RShape, @AO5RDanielsson, @AO5RHaralick, @AO5RMalinowska, @AO5TopEuler, @AO5BottomEuler,  @AO5Euler, @Centroid1,@Centroid2, @Eccentricity, @ConvexArea, @Solidity, @Extent, @Perimeter};
-    w = zeros(len, length(fun));
+    res = zeros(len, length(fun));
     for x = 1:len
-        img = double(readimage(ids, x))/255;
+        img = double(readimage(imgs, x))/255;
         img = img(:,:,1);
         img = ~imbinarize(img,0.95);
         img = imopen(img,ones(2));
@@ -13,7 +13,7 @@ function w = wczytaj(ids)
                 binary_image = a(i).Image;
                 binary_image = padarray(binary_image,0);
                 binary_image = imresize(binary_image, [200, 200]);
-                w(x,j) =  fun{j}(binary_image);
+                res(x,j) =  fun{j}(binary_image);
             end
         end
     end
--- funkcje/train_nn.m
+++ funkcje/train_nn.m
@@ -1,45 +1,45 @@
-function wsp = wspolczynniki_train()
-    ids_x = imageDatastore('letters/x');
-    ids_y = imageDatastore('letters/y');
-    ids_1 = imageDatastore('number/num_1');
-    ids_2 = imageDatastore('number/num_2');
-    ids_3 = imageDatastore('number/num_3');
-    ids_4 = imageDatastore('number/num_4');
-    ids_5 = imageDatastore('number/num_5');
-    ids_6 = imageDatastore('number/num_6');
-    ids_7 = imageDatastore('number/num_7');
-    ids_8 = imageDatastore('number/num_8');
-    ids_9 = imageDatastore('number/num_9');
-    ids_0 = imageDatastore('number/num_0');
-    ids_add = imageDatastore('operations/oper_addition');
-    ids_div = imageDatastore('operations/oper_division');
-    ids_multi = imageDatastore('operations/oper_multiplication');
-    ids_sub = imageDatastore('operations/oper_subtraction');
-    ids_left_arr = imageDatastore('operations/oper_left_arrow');
-    ids_right_arr = imageDatastore('operations/oper_right_arrow');
-    ids_expon = imageDatastore('operations/oper_exponentiation');
+function train_nn()
+    imgs_x = imageDatastore('letters/x');
+    imgs_y = imageDatastore('letters/y');
+    imgs_1 = imageDatastore('number/num_1');
+    imgs_2 = imageDatastore('number/num_2');
+    imgs_3 = imageDatastore('number/num_3');
+    imgs_4 = imageDatastore('number/num_4');
+    imgs_5 = imageDatastore('number/num_5');
+    imgs_6 = imageDatastore('number/num_6');
+    imgs_7 = imageDatastore('number/num_7');
+    imgs_8 = imageDatastore('number/num_8');
+    imgs_9 = imageDatastore('number/num_9');
+    imgs_0 = imageDatastore('number/num_0');
+    imgs_add = imageDatastore('operations/oper_addition');
+    imgs_div = imageDatastore('operations/oper_division');
+    imgs_multi = imageDatastore('operations/oper_multiplication');
+    imgs_sub = imageDatastore('operations/oper_subtraction');
+    imgs_left_arr = imageDatastore('operations/oper_left_arrow');
+    imgs_right_arr = imageDatastore('operations/oper_right_arrow');
+    imgs_expon = imageDatastore('operations/oper_exponentiation');
 
 
-    w1 = wczytaj(ids_1);
-    w2 = wczytaj(ids_2);
-    w3 = wczytaj(ids_3);
-    w4 = wczytaj(ids_4);
-    w5 = wczytaj(ids_5);
-    w6 = wczytaj(ids_6);
-    w7 = wczytaj(ids_7);
-    w8 = wczytaj(ids_8);
-    w9 = wczytaj(ids_9);
-    w0 = wczytaj(ids_0);
+    w1 = process_imgs(imgs_1);
+    w2 = process_imgs(imgs_2);
+    w3 = process_imgs(imgs_3);
+    w4 = process_imgs(imgs_4);
+    w5 = process_imgs(imgs_5);
+    w6 = process_imgs(imgs_6);
+    w7 = process_imgs(imgs_7);
+    w8 = process_imgs(imgs_8);
+    w9 = process_imgs(imgs_9);
+    w0 = process_imgs(imgs_0);
 
-    wadd = wczytaj(ids_add);
-    wdiv = wczytaj(ids_div);
-    wmulti = wczytaj(ids_multi);
-    wsub = wczytaj(ids_sub);
-    wlarr = wczytaj(ids_left_arr);
-    wrarr = wczytaj(ids_right_arr);
-    wexp = wczytaj(ids_expon);
-     wx = wczytaj(ids_x);
-     wy = wczytaj(ids_y);
+    wadd = process_imgs(imgs_add);
+    wdiv = process_imgs(imgs_div);
+    wmulti = process_imgs(imgs_multi);
+    wsub = process_imgs(imgs_sub);
+    wlarr = process_imgs(imgs_left_arr);
+    wrarr = process_imgs(imgs_right_arr);
+    wexp = process_imgs(imgs_expon);
+     wx = process_imgs(imgs_x);
+     wy = process_imgs(imgs_y);
 
     trainin = [w1;w2;w3;w4;w5;w6;w7;w8;w9;w0;wadd;wdiv;wmulti;wsub;wlarr;wrarr;wexp;wx;wy]';
 
@@ -75,6 +75,6 @@ nn.trainFcn = 'trainbr';
 nn.performFcn = 'mse';
 nn = train(nn,trainin,trainout);
 
-ocr_final_8_new = nn;
-save ocr_final_8_new;
+new_neural_network = nn;
+save new_neural_netwoek;
 end
